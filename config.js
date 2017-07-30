27517d1776e16fa4cac2cd6c2baba63a4d7c7b6d
Mark Poepping <mark.poepping@mmpro.de>
2017-07-30 10:41:35 +0200

    Minor path fix

--- config.js
+++ config.js
@@ -2,8 +2,9 @@
  * Origin: https://github.com/conventional-changelog/standard-version/issues/154
  */
 const fs = require('fs')
-const commit =  fs.readFileSync('./templates/commit.hbs', 'utf-8')
-const header =  fs.readFileSync('./templates/header.hbs', 'utf-8')
+const nodeModulePath = './node_modules/ac-conventional-changelog-template/'
+const commit =  fs.readFileSync(nodeModulePath + 'templates/commit.hbs', 'utf-8')
+const header =  fs.readFileSync(nodeModulePath + 'templates/header.hbs', 'utf-8')
 
 module.exports = {
   "changelogOpts": {
